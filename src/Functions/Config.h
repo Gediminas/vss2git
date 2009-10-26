90388958995c42e1a10f9f199186f5877ed5cf66
Gediminas <Gediminas@.(none)>
2009-10-26 20:03:37 +0200

    some functions renamed

--- src/Functions/Config.h
+++ src/Functions/Config.h
@@ -5,11 +5,11 @@
 
 namespace paths
 {
-	const LPCTSTR szTmpDir        = "../tmp";
-	const LPCTSTR szWorkingDir    = "../tmp/Working";
-	const LPCTSTR szOutput1_Vss   = "../tmp/1_OutputVss.txt";
-	const LPCTSTR szOutput2_Paths = "../tmp/2_OutputPaths.txt";
-	const LPCTSTR szOutputVersions = "../tmp/OutputVersions.txt";
+	const LPCTSTR szTmpDir       = "../tmp";
+	const LPCTSTR szWorkingDir   = "../tmp/Working";
+	const LPCTSTR szStep1_VssDir = "../tmp/Step1_VssDir.txt";
+	const LPCTSTR szStep2_Paths  = "../tmp/Step2_Paths.txt";
+	const LPCTSTR szStep3_Info   = "../tmp/Step3_Info.txt";
 
 }
 
--- src/Functions/Processor.cpp
+++ src/Functions/Processor.cpp
@@ -32,8 +32,16 @@ static void Initialize()
 	//system(ss + " History $/Wood/MatrixKozijn/StdAfx.h >>..\\tmp\\history.txt");
 }
 
-static void CollectPaths(SDataVect &vect, LPCTSTR szInputFile, LPCTSTR szOutputFile)
+static void Step1_VssPaths(LPCTSTR szOutputFile)
 {
+	printf("\nSTEP1\n");
+	vss::list_all_files(szOutputFile);
+}
+
+static void Step2_CollectPaths(LPCTSTR szInputFile, LPCTSTR szOutputFile)
+{
+	printf("\nSTEP2\n");
+
 	if (!file::StartJob(szOutputFile))
 	{
 		CStdioFile file;
@@ -90,19 +98,76 @@ static void CollectPaths(SDataVect &vect, LPCTSTR szInputFile, LPCTSTR szOutputF
 	}
 };
 
+static void Step3_CollectAllInfo(LPCTSTR szInputFile, LPCTSTR szOutputFile)
+{
+	printf("\nSTEP3\n");
+//	if (!file::StartJob(szOutputFile))
+//	{
+//		CStdioFile file;
+//		CFileException fe;
+//		if (!file.Open(szInputFile, CFile::modeRead | CFile::shareDenyWrite, &fe))
+//		{
+//			printf("file error\n");
+//			exit(1);
+//		
+//		}
+//
+//		const DWORD dwFileLength = file.GetLength();
+//
+//		CString sLine;
+//		CString sCurrentFolder;
+//
+//		while (file.ReadString(sLine))
+//		{
+//			if (sLine.IsEmpty())
+//			{
+//				continue;
+//			}
+//
+//			if (-1 != sLine.Find("$/"))
+//			{
+//				//FOLDER 'CHANGED'
+//				sCurrentFolder = sLine;
+//
+//				while (':' != sCurrentFolder[sCurrentFolder.GetLength()-1])
+//				{
+//					if (!file.ReadString(sLine))
+//					{
+//						ASSERT(FALSE);
+//						return;
+//					}
+//
+//					sCurrentFolder += sLine;
+//				}
+//
+//				sCurrentFolder.Delete(sCurrentFolder.GetLength()-1); //delete ':'
+//
+//				printf("\r>> %d%%", 100 * file.GetPosition() / dwFileLength);
+//			}
+//			else if (-1 != sLine.Find(".cpp") ||
+//					(-1 != sLine.Find(".h"))   )
+//			{
+//				//ADD FILE
+//				system(CString("ECHO ") + sCurrentFolder + "/" + sLine + " >> " + szOutputFile);
+//			}
+//		}
+//
+//		file.Close();
+//		file::MarkJobDone(szOutputFile);
+//	}
+};
+
 
 void processor::Run()
 {
 	printf("\nINIT\n");
 	Initialize();
 
-	printf("\nSTEP1\n");
-	vss::list_all_files(paths::szOutput1_Vss);
+	Step1_VssPaths      (paths::szStep1_VssDir);
+	Step2_CollectPaths  (paths::szStep1_VssDir, paths::szStep2_Paths);
+	Step3_CollectAllInfo(paths::szStep2_Paths,  paths::szStep3_Info);
 
-	printf("\nSTEP2\n");
-	SDataVect vect;
-	CollectPaths(vect, paths::szOutput1_Vss, paths::szOutput2_Paths);
-
+	//SDataVect vect;
 }
 
 
